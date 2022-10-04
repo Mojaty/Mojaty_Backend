c4cc497c111e69c67667b39f7429d658449d8f40
nswon <swnam0729@gmail.com>
2022-10-05 08:46:09 +0900

    fix :: Users Entity

--- src/main/java/com/motivation/users/domain/Users.java
+++ src/main/java/com/motivation/users/domain/Users.java
@@ -1,6 +1,5 @@
 package com.motivation.users.domain;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,25 +7,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
+import static javax.persistence.EnumType.*;
+import static javax.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 public class Users {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "users_id")
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false)
     private String email;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(STRING)
     private Job job;
 
     @Column(nullable = false)
     private String password;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(STRING)
     private Role role;
 
     @Builder
