aa736f1db56a876be142725cd1f0442ce9a5bd18
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-05 07:59:25 +0900

    FEAT :: Exception을 Global하게 처리

---
+++ src/main/java/com/motivation/mojaty/global/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.motivation.mojaty.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR(500, "서버에 오류가 발생했습니다."),
+    BAD_REQUEST(400, "잘못된 요청입니다."),
+
+    USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
+
+    ALREADY_EXISTS_USER(422, "이미 존재하는 회원입니다."),
+    NOT_MATCH_PASSWORD(401, "비밀번호가 일치하지 않습니다."),
+
+    USER_NOT_LOGIN(403, "로그인 후 이용해주세요"),
+
+    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
+    INVALID_TOKEN(401, "무효한 토큰입니다."),
+    NOT_MATCH_TOKEN(402, "토큰이 일치하지 않습니다."),
+    RETRY_LOGIN(402, "로그인을 다시 해주세요.");
+
+    private final int status;
+    private final String message;
+}
---
+++ src/main/java/com/motivation/mojaty/global/exception/GlobalException.java
@@ -0,0 +1,50 @@
+package com.motivation.mojaty.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalException {
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    @ExceptionHandler(CustomException.class)
+    public ErrorResponse handleException(CustomException e, HttpServletRequest request) {
+        log.error("\nERROR_CODE: {} \n접근 경로: {} \nMessage: {}",
+                e.getErrorCode(), request.getRequestURI(), e.getErrorCode().getMessage());
+
+        return ErrorResponse.builder()
+                .status(e.getErrorCode().getStatus())
+                .message(e.getErrorCode().getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleBadRequest(MethodArgumentNotValidException e, HttpServletRequest request) {
+        log.error("url: {}, message: {}", request.getRequestURI(), e.getMessage());
+
+        return ErrorResponse.builder()
+                .status(ErrorCode.BAD_REQUEST.getStatus())
+                .message(
+                        e.getBindingResult().getFieldErrors().get(0).getField() + "의 " +
+                                e.getBindingResult().getFieldErrors().get(0).getDefaultMessage())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e, HttpServletRequest request) {
+        log.error("url: {}, message: {}", request.getRequestURI(), e.getMessage());
+
+        return ErrorResponse.builder()
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .message(ErrorCode.INTERNAL_SERVER_ERROR.getMessage())
+                .build();
+    }
+
+}
